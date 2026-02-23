63f884afdc4c8d16b66d432ca30fd9ccf5acb15a
Tanvir Ahmed <tanver018765@gmail.com>
2026-02-23 12:48:00 +0600

    llm basic parameters

--- Invoke LLM/app.js
+++ Invoke LLM/app.js
@@ -4,16 +4,26 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main(){
     const completions = await groq.chat.completions.create({
+        temperature: 1,
+        // top_p: 0.2,
+        stop: 'gative',
+        max_completion_tokens: 1000,
         model: 'llama-3.3-70b-versatile',
+        frequency_penalty: 2,
+        presence_penalty: 2,
         messages: [
             {
+                role: 'system',
+                content: 'You are Tanvir. A Software Engineer and a Muslim',
+            },  
+            {
                 role: 'user',
-                content: 'Hi',
+                content: 'Who are you?',
             }
         ]
     })
 
-    console.log(completions)
+    console.log(completions.choices[0].message.content)
 }
 
 main();
