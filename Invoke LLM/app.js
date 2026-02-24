8c5fba5181d68b49a5c404b11b2ff17ba0103a24
Tanvir Ahmed <tanver018765@gmail.com>
2026-02-24 14:32:49 +0600

    response_format implement

--- Invoke LLM/app.js
+++ Invoke LLM/app.js
@@ -6,19 +6,20 @@ async function main(){
     const completions = await groq.chat.completions.create({
         temperature: 1,
         // top_p: 0.2,
-        stop: 'gative',
-        max_completion_tokens: 1000,
-        frequency_penalty: 2,
-        presence_penalty: 2,
+        // stop: 'gative',
+        // max_completion_tokens: 1000,
+        // frequency_penalty: 2,
+        // presence_penalty: 2,
+        response_format: {'type': 'json_object'},
         model: 'llama-3.3-70b-versatile',
         messages: [
             {
                 role: 'system',
-                content: 'You are Tanvir. A Software Engineer and a Muslim',
+                content: `You are a islamic scholar. You will be asked questions from the Hadith and the Quran. You will give the answers briefly. You must provide the answer in json format.`,
             },  
             {
                 role: 'user',
-                content: 'Who are you?',
+                content: 'Should I cross my hands on my chest or below my navel during prayer?',
             }
         ]
     })
