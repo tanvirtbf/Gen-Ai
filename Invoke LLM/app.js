e5dadfd99b8c7eea67f16f184b553da419505937
Tanvir Ahmed <tanver018765@gmail.com>
2026-03-11 10:19:28 +0600

    let's setup tool calling

--- Invoke LLM/app.js
+++ Invoke LLM/app.js
@@ -4,20 +4,17 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY })
 
 async function main(){
   const completions = await groq.chat.completions.create({
-    temperature: 1, 
+    temperature: 0, 
     model: 'llama-3.3-70b-versatile',
-    response_format: {'type': 'json_object'},
+    // response_format: {'type': 'json_object'},
     messages: [
       {
         role: 'system',
-        content: `You are a teacher. your student asked some question and you must be answer it by json object data. like : 
-        {
-          'What is the name of the national fruit of Bangladesh?? ' : 'Jackfruit'
-        }`
+        content: `You are a smart personal assistant who answers the asked questions`
       },
       {
         role: 'user',
-        content: 'What is the name of the Prime Minister of Bangladesh? '
+        content: 'When was Iphone 17 Launched?  '
       }
     ]
   })
