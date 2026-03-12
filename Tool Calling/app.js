6650c9e08842c0e452772c0263d86ee995847f68
Tanvir Ahmed <tanver018765@gmail.com>
2026-03-12 15:13:27 +0600

    update

--- Tool Calling/app.js
+++ Tool Calling/app.js
@@ -42,6 +42,26 @@ async function main() {
     tool_choice: 'auto'
   });
 
+  const toolCalls = completions.choices[0].message.tool_calls
+
+  if(!toolCalls) {
+    console.log(`Assistant: ${completions.choices[0].message.content}`)
+    return 
+  }
+
+  for(const tool of toolCalls) {
+    console.log('tool', tool)
+    const functionName = tool.function.name;
+    const functionParams = tool.function.arguments;
+
+    if(functionName === 'webSearch') {
+      const toolResult = await webSearch(JSON.parse(functionParams));
+      console.log('tool Result : ', toolResult)
+      return toolResult
+    }
+
+  }
+
   console.log("result: ", JSON.stringify(completions.choices[0].message, null , 2 ));
 }
 
@@ -49,6 +69,7 @@ main();
 
 async function webSearch({ query }) {
   // Here We will do tavily api call
+  console.log("calling web search")
 
   return "Iphone was lauched on 20 september 2024. ";
 }
