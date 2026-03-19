79792ca2f3c9862e3dbd4cd25ee960a35d87fb70
Tanvir Ahmed <tanver018765@gmail.com>
2026-03-19 17:43:54 +0600

    feat: integrate tavily API for enhanced web search functionality

--- Tool Calling/app.js
+++ Tool Calling/app.js
@@ -1,6 +1,9 @@
 import Groq from "groq-sdk";
+import { tavily } from "@tavily/core";
+import { process } from "zod/v4/core";
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+const tvly = new tavily({ apiKey: process.env.TAVILY_API_KEY });
 
 async function main() {
   const completions = await groq.chat.completions.create({
@@ -24,52 +27,55 @@ async function main() {
         type: "function",
         function: {
           name: "webSearch",
-          description: "Search the latest information and realtime data on the internet.",
+          description:
+            "Search the latest information and realtime data on the internet.",
           parameters: {
             // JSON Schema object
             type: "object",
             properties: {
               query: {
-                type: 'string',
-                description: 'The search query to perform search on.'
-              }
+                type: "string",
+                description: "The search query to perform search on.",
+              },
             },
             required: ["query"],
           },
         },
       },
     ],
-    tool_choice: 'auto'
+    tool_choice: "auto",
   });
 
-  const toolCalls = completions.choices[0].message.tool_calls
+  const toolCalls = completions.choices[0].message.tool_calls;
 
-  if(!toolCalls) {
-    console.log(`Assistant: ${completions.choices[0].message.content}`)
-    return 
+  if (!toolCalls) {
+    console.log(`Assistant: ${completions.choices[0].message.content}`);
+    return;
   }
 
-  for(const tool of toolCalls) {
-    console.log('tool', tool)
+  for (const tool of toolCalls) {
+    console.log("tool", tool);
     const functionName = tool.function.name;
     const functionParams = tool.function.arguments;
 
-    if(functionName === 'webSearch') {
+    if (functionName === "webSearch") {
       const toolResult = await webSearch(JSON.parse(functionParams));
-      console.log('tool Result : ', toolResult)
-      return toolResult
+      console.log("tool Result : ", toolResult);
+      return toolResult;
     }
-
   }
 
-  console.log("result: ", JSON.stringify(completions.choices[0].message, null , 2 ));
+  console.log(
+    "result: ",
+    JSON.stringify(completions.choices[0].message, null, 2),
+  );
 }
 
 main();
 
 async function webSearch({ query }) {
   // Here We will do tavily api call
-  console.log("calling web search")
+  console.log("calling web search");
 
   return "Iphone was lauched on 20 september 2024. ";
 }
