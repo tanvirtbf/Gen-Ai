96a93e622653fd1debad34b37761fc67ea7672f6
Tanvir Ahmed <tanver018765@gmail.com>
2026-03-20 01:46:18 +0600

    refactor: restructure main function and enhance user interaction flow

--- Tool Calling/app.js
+++ Tool Calling/app.js
@@ -1,112 +1,101 @@
+import readline from "node:readline/promises";
 import Groq from "groq-sdk";
 import { tavily } from "@tavily/core";
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 const tvly = new tavily({ apiKey: process.env.TAVILY_API_KEY });
 
-const messages = [
-  {
-    role: "system",
-    content: `You are a smart personal assistant who answers the asked questions. 
-        You have access to following tools: 
-        1. webSearch({ query }: {query: string}) // Search the latest information and realtime data on the internet.`,
-  },
-  {
-    role: "user",
-    content: "When was Iphone 17 Launched?  ",
-  },
-];
-
 async function main() {
-  const completions = await groq.chat.completions.create({
-    temperature: 0,
-    model: "llama-3.3-70b-versatile",
-    // response_format: {'type': 'json_object'},
-    messages: messages,
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description:
-            "Search the latest information and realtime data on the internet.",
-          parameters: {
-            // JSON Schema object
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "The search query to perform search on.",
+  const rl = await readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const messages = [
+    {
+      role: "system",
+      content: `You are a smart personal assistant who answers the asked questions. 
+          You have access to following tools: 
+          1. webSearch({ query }: {query: string}) // Search the latest information and realtime data on the internet.`,
+    },
+    // {
+    //   role: "user",
+    //   content: "When was Iphone 17 Launched?  ",
+    // },
+  ];
+
+  while (true) {
+    const question = await rl.question("You : ");
+    if (question === "bye") {
+      break;
+    }
+    messages.push({
+      role: "user",
+      content: question,
+    });
+    
+    while (true) {
+      const completions = await groq.chat.completions.create({
+        temperature: 0,
+        model: "llama-3.3-70b-versatile",
+        // response_format: {'type': 'json_object'},
+        messages: messages,
+        tools: [
+          {
+            type: "function",
+            function: {
+              name: "webSearch",
+              description:
+                "Search the latest information and realtime data on the internet.",
+              parameters: {
+                // JSON Schema object
+                type: "object",
+                properties: {
+                  query: {
+                    type: "string",
+                    description: "The search query to perform search on.",
+                  },
+                },
+                required: ["query"],
               },
             },
-            required: ["query"],
           },
-        },
-      },
-    ],
-    tool_choice: "auto",
-  });
-
-  messages.push(completions.choices[0].message);
-
-  const toolCalls = completions.choices[0].message.tool_calls;
-
-  if (!toolCalls) {
-    console.log(`Assistant: ${completions.choices[0].message.content}`);
-    return;
-  }
-
-  for (const tool of toolCalls) {
-    console.log("tool", tool);
-    const functionName = tool.function.name;
-    const functionParams = tool.function.arguments;
-
-    if (functionName === "webSearch") {
-      const toolResult = await webSearch(JSON.parse(functionParams));
-      console.log("tool Result : ", toolResult);
-
-      messages.push({
-        tool_call_id: tool.id,
-        role: "tool",
-        name: functionName,
-        content: toolResult,
+        ],
+        tool_choice: "auto",
       });
+
+      messages.push(completions.choices[0].message);
+
+      const toolCalls = completions.choices[0].message.tool_calls;
+
+      if (!toolCalls) {
+        console.log(`Assistant: ${completions.choices[0].message.content}`);
+        break;
+      }
+
+      for (const tool of toolCalls) {
+        // console.log("tool", tool);
+        const functionName = tool.function.name;
+        const functionParams = tool.function.arguments;
+
+        if (functionName === "webSearch") {
+          const toolResult = await webSearch(JSON.parse(functionParams));
+          // console.log("tool Result : ", toolResult);
+
+          messages.push({
+            tool_call_id: tool.id,
+            role: "tool",
+            name: functionName,
+            content: toolResult,
+          });
+        }
+      }
+
+      // console.log(
+      //   "result: ",
+      //   JSON.stringify(completions.choices[0].message, null, 2),
+      // );
     }
   }
-
-  const completions2 = await groq.chat.completions.create({
-    temperature: 0,
-    model: "llama-3.3-70b-versatile",
-    // response_format: {'type': 'json_object'},
-    messages: messages,
-    tools: [
-      {
-        type: "function",
-        function: {
-          name: "webSearch",
-          description:
-            "Search the latest information and realtime data on the internet.",
-          parameters: {
-            // JSON Schema object
-            type: "object",
-            properties: {
-              query: {
-                type: "string",
-                description: "The search query to perform search on.",
-              },
-            },
-            required: ["query"],
-          },
-        },
-      },
-    ],
-    tool_choice: "auto",
-  });
-
-  console.log(
-    "result: ",
-    JSON.stringify(completions2.choices[0].message, null, 2),
-  );
 }
 
 main();
@@ -116,10 +105,10 @@ async function webSearch({ query }) {
   console.log("calling web search");
 
   const response = await tvly.search(query, { max_results: 5 });
-  console.log("response : ", response);
+  // console.log("response : ", response);
 
   const finalResult = response.results.map((item) => item.content).join("/n/n");
-  console.log("final result : ", finalResult);
+  // console.log("final result : ", finalResult);
 
   return finalResult;
 }
