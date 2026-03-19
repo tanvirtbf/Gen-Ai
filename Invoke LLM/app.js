01ca8fe28fe62fa04f5af3f5b52af0f46ee8534a
Tanvir Ahmed <tanver018765@gmail.com>
2026-03-19 17:00:05 +0600

    refactor: improve code formatting and consistency in app.js

--- Invoke LLM/app.js
+++ Invoke LLM/app.js
@@ -1,74 +1,54 @@
-import Groq from 'groq-sdk'
+import Groq from "groq-sdk";
 
-const groq = new Groq({ apiKey: process.env.GROQ_API_KEY })
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
-async function main(){ 
+async function main() {
   const completions = await groq.chat.completions.create({
     temperature: 0,
-    model: 'llama-3.3-70b-versatile',
+    model: "llama-3.3-70b-versatile",
+    // response_format: {'type': 'json_object'},
     messages: [
       {
-        role: 'system',
+        role: "system",
         content: `You are a smart personal assistant who answers the asked questions. 
         You have access to following tools: 
-        1. webSearch({ query }: {query: string}) // Search the latest information and realtime data on the internet.`
+        1. webSearch({ query }: {query: string}) // Search the latest information and realtime data on the internet.`,
       },
       {
-        role: 'user',
-        content: 'When was Iphone 17 Launched?'
-      }
+        role: "user",
+        content: "When was Iphone 17 Launched?  ",
+      },
     ],
     tools: [
       {
-        type: 'function',
+        type: "function",
         function: {
-          name: 'webSearch',
-          description: 'Search the latest information and realtime data on the internet.',
+          name: "webSearch",
+          description: "Search the latest information and realtime data on the internet.",
           parameters: {
-            type: 'object',
+            // JSON Schema object
+            type: "object",
             properties: {
               query: {
                 type: 'string',
                 description: 'The search query to perform search on.'
               }
             },
-            required: ['query']
+            required: ["query"],
           },
-        }
-      }
+        },
+      },
     ],
     tool_choice: 'auto'
-  })
+  });
 
-  const toolCalls = completions.choices[0].message.tool_calls 
-
-  if(!toolCalls) {
-    console.log(`AI Assistant Say : ${completions.choices[0].message.content}`)
-
-    return 
-  }
-
-  for(let tool of toolCalls) {
-    console.log('tools: ', tool)
-    const functionName = tool.function.name
-    const argumentsObject = JSON.parse(tool.function.arguments)
-    console.log(functionName)
-    console.log(argumentsObject.query)
-
-    if(functionName === 'webSearch') {
-      const finalResult = webSearch(argumentsObject)
-      console.log('final result : ', finalResult)
-      return 
-    }
-  }
-
-  // console.log('result : ', JSON.stringify(completions.choices[0].message, null , 2))
+  console.log("result: ", JSON.stringify(completions.choices[0].message, null , 2 ));
 }
 
-main()
+main();
 
- function webSearch({query}) {
-  console.log('query : ', query)
+async function webSearch({ query }) {
+  // Here We will do tavily api call
 
-  return 'Iphone 17 Launch in 10 March 2026'
+  return "Iphone was lauched on 20 september 2024. ";
 }
