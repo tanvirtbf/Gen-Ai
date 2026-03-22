d0e508cef581d54d00f6584b31b0e891ec3ec271
Tanvir Ahmed <tanver018765@gmail.com>
2026-03-22 19:46:37 +0600

    feat: enhance server functionality with chat endpoint and JSON parsing

--- ChatGpt Clone/server.js
+++ ChatGpt Clone/server.js
@@ -1,11 +1,22 @@
-import express from 'express'
+import express from "express";
 
 const app = express();
-const port = 3001
+const port = 3001;
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+  res.send("Hello World!");
+});
+
+app.post("/chat", (req, res) => {
+  const { message } = req.body;
+
+  console.log("message: ", message);
+
+  res.json({ message: message });
+});
 
 app.listen(port, () => {
-    console.log('Server Start!')
-})
+  console.log("Server Start!");
+});
