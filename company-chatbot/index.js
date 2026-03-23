0bc469c2297e770e342eb61f9185285fb238f859
Tanvir Ahmed <tanver018765@gmail.com>
2026-03-23 23:17:55 +0600

    project plan compelte

--- company-chatbot/index.js
+++ company-chatbot/index.js
@@ -1,1 +1,16 @@
+/**
+ * Implementation plan
+ * Stage 1: Indexing
+ * 1. Load the document - pdf, text
+ * 2. Chunk the document
+ * 3. Generate vector embeddings
+ * 4. Store the vector embeddings - Vector database
+ *
+ * Stage 2: Using the chatbot
+ * 1. Setup LLM
+ * 2. Add retrieval step
+ * 3. Pass input + relevant information to LLM
+ *
+ */
+
 console.log("Hello via Bun!");
