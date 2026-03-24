a6e27b43d23d14285a26bd4b66845d7e9cfbdfe7
Tanvir Ahmed <tanver018765@gmail.com>
2026-03-24 17:34:10 +0600

    complete Generate vector embeddings and Indexing

--- company-chatbot/prepare.js
+++ company-chatbot/prepare.js
@@ -1,7 +1,24 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { Document } from "@langchain/core/documents";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { PineconeStore } from "@langchain/pinecone";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
 
+// Flow : prothome document jetar moddhe companyr data thake seta hote pare pdf or onno kono format e sei documents chunk chunk kore vag kore nite hobe. karon ami chai jokhon jeta chauya hobe tokhon sei chunk retrieval er somoy pathano hobe. then chunk kora data ke aro 2 ta phase diye jaite hoy . 1. embeddings and 2. store into vector database . kintu ai 2 ta phase langchain use kore korle se nijeii behind the scena kore fele . then sei vector store theke data retrieve er somoy pathano hoy . 
+
+const embeddings = new OpenAIEmbeddings({
+  model: "text-embedding-3-small",
+  // dimensions: 1536, // this is optional
+}); // ekhane api key deya jay . jemon new OpenAIEmbeddings({ apiKey: ..}) . kintu jodi na dei tahole behind the scene automatic .env theke api key niye ney but shorto hocche name exactly rakhte hobe eta : "OPENAI_API_KEY"
+
+const pinecone = new PineconeClient(); // ekhane api key deya jay . jemon new PineconeClient({ apiKey: ..}) . kintu jodi na dei tahole behind the scene automatic .env theke api key niye ney but shorto hocche name exactly rakhte hobe eta : "PINECONE_API_KEY"
+
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX_NAME);
+export const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+  pineconeIndex,
+  maxConcurrency: 5,
+});
 export async function indexTheDocument(filePath) {
   const loader = new PDFLoader(filePath, { splitPages: false });
 
@@ -15,5 +32,18 @@ export async function indexTheDocument(filePath) {
 
   const texts = await textsplitter.splitText(doc[0].pageContent);
 
-  console.log("texts : ", texts);
+  const documents = texts.map((chunk) => {
+    return new Document({
+      pageContent: chunk,
+      metadata: doc[0].metadata,
+    });
+  });
+
+  // Data retrieve
+  await vectorStore.addDocuments(documents);
+
+  // For all data remove
+  // await pineconeIndex.namespace("").deleteAll();
+
+  // console.log("documents : ", documents.length);
 }
