5b29ff247684c95fdc416334a5193bb227014e1e
Tanvir Ahmed <tanver018765@gmail.com>
2026-03-26 23:30:30 +0600

    final refactor

--- Saas Chatbot Project/backend/agents/chatAgent.js
+++ Saas Chatbot Project/backend/agents/chatAgent.js
@@ -3,26 +3,26 @@ import { MemorySaver } from "@langchain/langgraph";
 import { TavilySearch } from "@langchain/tavily";
 import { createAgent } from "langchain";
 
+const model = new ChatGroq({
+  model: "openai/gpt-oss-120b",
+  maxTokens: undefined,
+  maxRetries: 3,
+});
+
+const search = new TavilySearch({
+  maxResults: 3,
+  topic: "general",
+});
+
+const checkpointer = new MemorySaver();
+
+const agent = createAgent({
+  model: model,
+  tools: [search],
+  checkpointer: checkpointer,
+});
+
 export async function chatAgent(query) {
-  const model = new ChatGroq({
-    model: "openai/gpt-oss-120b",
-    maxTokens: undefined,
-    maxRetries: 3,
-  });
-
-  const search = new TavilySearch({
-    maxResults: 3,
-    topic: "general",
-  });
-
-  const checkpointer = new MemorySaver();
-
-  const agent = createAgent({
-    model: model,
-    tools: [search],
-    checkpointer: checkpointer,
-  });
-
   const result = await agent.invoke(
     {
       messages: [
