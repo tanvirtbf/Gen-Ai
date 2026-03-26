637dd0e1b0015117d0f599a428373e91ed3ee07b
Tanvir Ahmed <tanver018765@gmail.com>
2026-03-26 22:54:10 +0600

    chatbot finally complete

--- Saas Chatbot Project/frontend/src/App.jsx
+++ Saas Chatbot Project/frontend/src/App.jsx
@@ -12,6 +12,7 @@ const fetchApi = async (query) => {
 
     const data = await response.json();
     console.log("Api Data: ", data);
+    return data.assistant;
   } catch (error) {
     console.error("API Error:", error);
   }
@@ -19,20 +20,27 @@ const fetchApi = async (query) => {
 
 const App = () => {
   const [query, setQuery] = useState("");
+  const [result, setResult] = useState("");
 
   function handleClick() {
-    fetchApi(query);
+    const res = fetchApi(query);
+    setResult(res);
   }
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Type Here.."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button onClick={handleClick}>Generate</button>
+    <div className="flex flex-col gap-4">
+      <div>
+        <h1>{result}</h1>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Type Here.."
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button onClick={handleClick}>Generate</button>
+      </div>
     </div>
   );
 };
