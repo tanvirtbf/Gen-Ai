54412b784f5ed69314455eab209f5828e360284e
Tanvir Ahmed <tanver018765@gmail.com>
2026-04-01 11:12:40 +0600

    LLM Setup complete

--- Google Calender Agent/index.ts
+++ Google Calender Agent/index.ts
@@ -1,8 +1,7 @@
-import { ChatGroq } from '@langchain/groq'
+import { ChatGroq } from "@langchain/groq";
 const tools: any = [];
 
-
 const model = new ChatGroq({
-    temperature: 2,
-    model: 'openai/gpt-oss-120b'
-})
+  temperature: 2,
+  model: "openai/gpt-oss-120b",
+}).bindTools(tools);
