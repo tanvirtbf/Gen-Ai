25f3c3be8bbc38c9ae22973799dedd231f8b4f12
Tanvir Ahmed <tanver018765@gmail.com>
2026-04-01 15:32:44 +0600

    Setup the Google Developer Console

--- Google Calender Agent/tools.ts
+++ Google Calender Agent/tools.ts
@@ -10,26 +10,32 @@ export const createEventTool = tool(
     name: "create-event",
     description: "Call to create the calender events",
     schema: z.object({
-      query: z.string().describe('The query to be used to create event into google calender')
+      query: z
+        .string()
+        .describe("The query to be used to create event into google calender"),
     }),
   },
 );
 
 export const getEventsTool = tool(
-    async () => {
-         // Google calender logic goes
-        return JSON.stringify([
-            {
-                title: 'Meeting with Sujoy',
-                date: '26th August 2025',
-                time: '2PM',
-                location: 'Dhaka'
-            }
-        ])
-    },
-    {
-        name: 'get-events',
-        description: 'Call to get the calender events',
-        schema: z.object({ query: z.string().describe('The query to be used to get events from google calender')})
-    }
-)
+  async () => {
+    // Google calender logic goes
+    return JSON.stringify([
+      {
+        title: "Meeting with Sujoy",
+        date: "26th August 2025",
+        time: "2PM",
+        location: "Dhaka",
+      },
+    ]);
+  },
+  {
+    name: "get-events",
+    description: "Call to get the calender events",
+    schema: z.object({
+      query: z
+        .string()
+        .describe("The query to be used to get events from google calender"),
+    }),
+  },
+);
