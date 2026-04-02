8cf96fcb1f689bec6df685a78143bea854d9c2c4
Tanvir Ahmed <tanver018765@gmail.com>
2026-04-02 12:08:08 +0600

    setup express server complete

--- Google Calender Agent/server.ts
+++ Google Calender Agent/server.ts
@@ -1,17 +1,47 @@
-import express from 'express'
+import express from "express";
+import { google } from "googleapis";
 
 const app = express();
 
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URL,
+);
+
 app.get("/auth", (req, res) => {
-    // Generate the link
+  // Generate the link
 
-    const link = ""
-    res.redirect(link)
-})
+  // generate a url that asks permissions for Blogger and Google Calendar scopes
+  const scopes = ["https://www.googleapis.com/auth/calendar"]; // er mane holo user er ki ki access nibo setar list . ekhane only calender er access nicchi
 
-app.get('/callback' , (req, res) => {
-    const code = req.query.code
+  const url = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: "offline", // online dile sudhu access token pauya jay. jokhon 1 hour por seta expire hoye jay tokhon user ke abaro permission dite hoy but offline dile refresh token soho dey jeta lifetime access pauya jay user er google calender er
 
-    // exchange code with access token / refresh token
-    res.send("Connected You can close this tab now")
+    prompt: "consent",
+
+    // If you only need one scope, you can pass it as a string
+    scope: scopes,
+  });
+
+  console.log("url: ", url);
+
+  res.redirect(url);
+});
+
+app.get("/callback", async (req, res) => {
+  const code = req.query.code as string;
+
+  const { tokens } = await oauth2Client.getToken(code);
+
+  console.log("tokens: ", tokens);
+
+  // exchange code with access token / refresh token
+  res.send("Connected You can close this tab now");
+});
+
+
+app.listen(3600, () => {
+    console.log(`Server is running on port: http://localhost:3600`)
 })
