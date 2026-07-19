8d79bc667a63731ea1c8fb01bd3706083ed955e2
Tanvir Ahmed <tanver018765@gmail.com>
2026-07-19 12:40:41 +0600

    Add dotenv dependency and update test.js to utilize environment variables

--- Invoke LLM/test.js
+++ Invoke LLM/test.js
@@ -1,30 +1,76 @@
+import "dotenv/config";
 import Groq from "groq-sdk";
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main() {
-  const completions = await groq.chat.completions.create({
+  const completions =await groq.chat.completions.create({
     model: "llama-3.3-70b-versatile",
-    temperature: 1, // range = 0-2
-    // top_p: 0.5, // range = 0-1
-    max_completion_tokens: 1000,
-    frequency_penalty: 2,
-    presence_penalty: 1,
+    temperature: 1,
     messages: [
       {
         role: "system",
-        content: `You are Jarvis. A smart review grader. Your task is to analyse given review and return the sentiment. Classify the review as positive, neutral or negative. You must return the result in valid JSON structure 
-        example: { "sentiment" : "Negative" }`,
+        content: `You are a smart personal assistant who answers the asked questions.`,
       },
       {
         role: "user",
-        content: `Review: These headphones arrived quickly and look great, but the left earcup stopped working after a week.
-        Sentiment: `,
+        content: "What is the current weather in dhaka?",
       },
     ],
   });
 
-  console.log("completions: ", completions.choices[0].message.content);
+  console.log(completions.choices[0].message.content);
 }
 
-main();
+await main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import "dotenv/config";
+// import Groq from "groq-sdk";
+
+// const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+// async function main() {
+//   const completions = await groq.chat.completions.create({
+//     model: "llama-3.3-70b-versatile",
+//     temperature: 1, // range = 0-2
+//     // top_p: 0.5, // range = 0-1
+//     max_completion_tokens: 1000,
+//     frequency_penalty: 2,
+//     presence_penalty: 1,
+//     messages: [
+//       {
+//         role: "system",
+//         content: `You are Jarvis. A smart review grader. Your task is to analyse given review and return the sentiment. Classify the review as positive, neutral or negative. You must return the result in valid JSON structure
+//         example: { "sentiment" : "Negative" }`,
+//       },
+//       {
+//         role: "user",
+//         content: `Review: These headphones arrived quickly and look great, but the left earcup stopped working after a week.
+//         Sentiment: `,
+//       },
+//     ],
+//   });
+
+//   console.log("completions: ", completions.choices[0].message.content);
+// }
+
+// main();
