7dbbc74928c6a7c38a07fed220afccfe1a285436
Tanvir Ahmed <tanver018765@gmail.com>
2026-07-22 16:53:49 +0600

    Integrate Tavily API for enhanced web search functionality and update message handling in main function

--- Invoke LLM/test.js
+++ Invoke LLM/test.js
@@ -1,24 +1,28 @@
 import "dotenv/config";
 import Groq from "groq-sdk";
+import { tavily } from "@tavily/core";
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
+
+const messages = [
+  {
+    role: "system",
+    content: `You are a smart personal assistant who answers the asked questions. 
+        You have access to following tools: 
+        1. searchWeb({query}: {query: string}) // Search the latest information and realtime data on the internet`,
+  },
+  {
+    role: "user",
+    content: "What is the current weather in dhaka?",
+  },
+];
 
 async function main() {
   const completions = await groq.chat.completions.create({
     model: "llama-3.3-70b-versatile",
     temperature: 1,
-    messages: [
-      {
-        role: "system",
-        content: `You are a smart personal assistant who answers the asked questions. 
-        You have access to following tools: 
-        1. searchWeb({query}: {query: string}) // Search the latest information and realtime data on the internet`,
-      },
-      {
-        role: "user",
-        content: "What is the current weather in dhaka?",
-      },
-    ],
+    messages: messages,
     tools: [
       {
         type: "function",
@@ -42,27 +46,64 @@ async function main() {
     tool_choice: "auto",
   });
 
-  const toolCalls = completions.choices[0].message.tool_calls
+  console.log('message 1 : ', JSON.stringify(completions.choices[0].message))
+  messages.push(completions.choices[0].message)
+  console.log('message 2 : ', JSON.stringify(completions.choices[0].message))
 
-  if(!toolCalls) {
-    console.log(`Assistant: ${completions.choices[0].message.content}`)
+  const toolCalls = completions.choices[0].message.tool_calls;
+
+  if (!toolCalls) {
+    console.log(`Assistant: ${completions.choices[0].message.content}`);
 
     return;
   }
 
-  for(const tool of toolCalls) {
-   console.log('tools: ', tool) 
-   const functionName = tool.function.name;
-   const params = tool.function.arguments;
+  for (const tool of toolCalls) {
+    console.log("tools: ", tool);
+    const functionName = tool.function.name;
+    const params = tool.function.arguments;
 
-   if(functionName==="webSearch"){
-    const result = await webSearch(JSON.parse(params))
-    console.log(result)
-   }
+    if (functionName === "webSearch") {
+      const result = await webSearch(JSON.parse(params));
+      // console.log(result);
+
+      messages.push({
+        tool_call_id: tool.id,
+        role: 'tool',
+        name: functionName,
+        content: result
+      })
+    }
   }
 
+  const completions2 = await groq.chat.completions.create({
+    model: "llama-3.3-70b-versatile",
+    temperature: 1,
+    messages: messages,
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "webSearch",
+          description:
+            "Search the latest information and realtime data on the internet",
+          parameters: {
+            type: "object",
+            properties: {
+              query: {
+                type: "string",
+                description: "The search query to perform search on",
+              },
+            },
+            required: ["query"],
+          },
+        },
+      },
+    ],
+    tool_choice: "auto",
+  });
 
-  // console.log(JSON.stringify(completions.choices[0].message));
+  // console.log(JSON.stringify(completions2.choices[0].message, null, 2));
 }
 
 await main();
@@ -70,22 +111,16 @@ await main();
 async function webSearch({ query }) {
   // Here we will do tavily api call
 
-  return "Iphone was launched on 20 september 2024";
+  const response = await tvly.search(query, { maxResults: 5 });
+  // console.log("response: ", response);
+
+  const finalResult = response.results
+    .map((result) => result.content)
+    .join("\n\n");
+  // console.log("final result", finalResult);
+
+  return finalResult;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- Invoke LLM/toolsetup.js
+++ Invoke LLM/toolsetup.js
@@ -1,6 +1,9 @@
+import "dotenv/config";
 import Groq from "groq-sdk";
+import { tavily } from "@tavily/core";
 
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+const tavily = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
 async function main() {
   const completions = await groq.chat.completions.create({
@@ -15,23 +18,46 @@ async function main() {
       },
       {
         role: "user",
-        content: "What is the current weather in dhaka?",
+        content: "What is the current weather in dhaka ?",
       },
     ],
     tools: [
-        {
-            type: 'function',
-            function: {
-                name: 'webSearch',
-                description: 'Search the latest information and realtime data on the internet',
-                parameters: {
-                    type: 'object',
-                    
-                }
-            }
-        }
-    ]
+      {
+        type: "function",
+        function: {
+          name: "webSearch",
+          description:
+            "Search the latest information and realtime data on the internet",
+          parameters: {
+            type: "object",
+            query: {
+              type: "string",
+              description: "The search query to perform search on",
+            },
+          },
+          required: ["query"],
+        },
+      },
+    ],
   });
+
+  const toolCalls = completions.choices[0].message.tool_calls;
+
+  if(!toolCalls) {
+    console.log(`Assistant : ${completions.choices[0].message.content}`)
+
+    return 
+  }
+
+  for(const tool of toolCalls) {
+    console.log(JSON.stringify(completions.choices[0].message.tool_calls))
+  }
+
+
+
+  // console.log(completions.choices[0].message);
+
+  
 }
 
 await main();
